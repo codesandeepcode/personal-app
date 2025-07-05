42b8da00e4bc2905fbe4a4fd3b9cb3cb6bbc528c
Sandeep Yadav <yadav.sandeep95@nic.in>
2025-07-05 11:32:20 +0000

    added home page

--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from .views import HomePageView
 
 urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
     path("areas/", include("apps.areas.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Welcome to Personal Life App"
+        return context
