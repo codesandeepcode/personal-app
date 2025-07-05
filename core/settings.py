4d57f6552e1f8711096aee71473e21516835f84c
Sandeep Yadav <yadav.sandeep95@nic.in>
2025-07-05 06:04:10 +0000

    added the apps in the settings.py file

--- core/settings.py
+++ core/settings.py
@@ -30,21 +30,37 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    "apps.finances",
-    "apps.accounts",
+DJANGO_APPS = (
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+)
+
+THIRD_PARTY_APPS = (
     "rest_framework",
     "rest_framework_simplejwt",
     "docs",
     "drf_spectacular",
     "drf_spectacular_sidecar",
-]
+)
+
+LOCAL_APPS = (
+    "apps.accounts",
+    "apps.areas",
+    "apps.family",
+    "apps.finances",
+    "apps.friends",
+    "apps.health",
+    "apps.hobbies",
+    "apps.romance",
+    "apps.spiritual",
+    "apps.work",
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
