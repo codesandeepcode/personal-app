932157f8a8049411a5501429f4db497d9743a92d
Sandeep Yadav <yadav.sandeep95@nic.in>
2025-07-05 12:31:08 +0000

    added signup and logout views

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,9 +1,22 @@
 from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from .serializers import UserSerializer
-from .forms import CustomAuthenticationForm
+from .forms import CustomAuthenticationForm, CustomUserCreationForm
 from rest_framework.response import Response
 
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        else:
+            return Response(form.errors, status=400)
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, "accounts/signup.html", {"form": form})
 
 class CustomLoginView(LoginView):
     authentication_form = CustomAuthenticationForm
